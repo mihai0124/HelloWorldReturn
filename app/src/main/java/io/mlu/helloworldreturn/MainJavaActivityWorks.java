56336d8f986713ba9d5038a970f80259760c2921
mihai0124 <mihai.luca@gmail.com>
2016-05-19 21:49:15 +0300

    primul pas

---
+++ app/src/main/java/io/mlu/helloworldreturn/MainJavaActivityWorks.java
@@ -0,0 +1,13 @@
+package io.mlu.helloworldreturn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainJavaActivityWorks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_java_activity_looks);
+    }
+}
